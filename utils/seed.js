1a0d6f88cacebef2b5c999694998d108fc44fa39
leviathan <jake66martin@hotmail.com>
2024-01-15 18:32:06 -0500

    working on seed file and index in models

---
+++ utils/seed.js
@@ -0,0 +1,15 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models')
+const data = require('./data')
+
+const dropCollectionIfExists = async(model)=>{
+    if ( await model.exists()) {
+        await model.collection.drop()
+    }
+}
+
+connection.once('open', async () => {
+
+    await dropCollectionIfExists(User)
+    await dropCollectionIfExists(Thought)
+})
