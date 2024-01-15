443b4ccc2db4f0cb75d33b1e35d3e30e242881dc
leviathan <jake66martin@hotmail.com>
2024-01-15 17:09:25 -0500

    created data and seed file

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes')
 const thoughtsRoutes = require('./thoughtRoutes')
 
-
 router.use('/user', userRoutes);
 router.use('/thoughts', thoughtsRoutes);
 
