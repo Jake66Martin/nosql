298229df8579828581a67803766becba8258e647
leviathan <jake66martin@hotmail.com>
2024-01-15 15:21:19 -0500

    created a user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+        "Invalid email format",
+      ],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thoughts" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "friends" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
